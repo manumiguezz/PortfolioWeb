6da280aa33e45ce7c80bc3cfb4857d1584e2d1b3
Manuel Miguez Lauría <manuelmiguezlauria@gmail.com>
2023-07-04 00:43:21 -0300

    custom sliverappbar finished

--- lib/widgets/widgets.dart
+++ lib/widgets/widgets.dart
@@ -1,4 +1,7 @@
+
+export 'package:personal_web/widgets/custom_sliverappbar.dart';
 export 'package:personal_web/widgets/home_scroll_view.dart';
 export 'package:personal_web/widgets/geometric_background.dart';
+
 
 
