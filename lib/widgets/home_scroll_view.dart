91c779dca709d5ed9e93a57d2ad6800bb94d524c
Manuel Miguez Lauría <manuelmiguezlauria@gmail.com>
2023-07-03 23:44:11 -0300

    scroll physics changed

--- lib/widgets/home_scroll_view.dart
+++ lib/widgets/home_scroll_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:personal_web/widgets/widgets.dart';
 
 class HomeScrollView extends StatelessWidget {
   const HomeScrollView({
@@ -8,20 +9,32 @@ class HomeScrollView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return CustomScrollView(
+      physics: const BouncingScrollPhysics(),
       slivers: [
         
         const SliverAppBar(
+          toolbarHeight: 80,
           backgroundColor: Colors.black,
-          centerTitle: true,
           pinned: true,
-          title: Text('Manuel Miguez Lauría',
-            style: TextStyle(fontFamily: 'madetommy', fontSize: 50),
+          elevation: 5,
+          expandedHeight: 760,
+          flexibleSpace: FlexibleSpaceBar(
+            centerTitle: true,
+            title: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('Manuel Miguez Lauría',
+                style: TextStyle(fontFamily: 'madetommy', fontSize: 50),
+                ),
+              ],
+            ),
           ),
         ),
 
         SliverList(
           delegate: SliverChildBuilderDelegate((context, index) {
-            return Column(
+            return const Column(
               children: [
                 Text('Deserunt cillum dolore veniam ipsum eiusmod velit nulla. Cillum qui reprehenderit mollit exercitation tempor magna adipisicing eiusmod excepteur quis mollit eiusmod et veniam. Laboris qui deserunt et cupidatat enim ex ex velit tempor dolore deserunt qui. Id sit nisi laboris est elit mollit deserunt ex labore labore sunt. Anim laboris aliquip dolor sit et in.')
               ],
--- lib/widgets/widgets.dart
+++ lib/widgets/widgets.dart
@@ -1,4 +1,4 @@
 export 'package:personal_web/widgets/home_scroll_view.dart';
-
+export 'package:personal_web/widgets/geometric_background.dart';
 
 
