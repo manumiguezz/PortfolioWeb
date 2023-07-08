790e6ccd815bdb22f173a1eefc4f81c06ac3e805
Manuel Miguez Lauría <manuelmiguezlauria@gmail.com>
2023-07-07 21:57:56 -0300

    added typewriter class (aguante el rojo)

--- lib/widgets/home_scroll_view.dart
+++ lib/widgets/home_scroll_view.dart
@@ -58,7 +58,10 @@ class HomeScrollView extends StatelessWidget {
           delegate: SliverChildBuilderDelegate((context, index) {
             return const Column(
               children: [
-                Text('Deserunt cillum dolore veniam ipsum eiusmod velit nulla. Cillum qui reprehenderit mollit exercitation tempor magna adipisicing eiusmod excepteur quis mollit eiusmod et veniam. Laboris qui deserunt et cupidatat enim ex ex velit tempor dolore deserunt qui. Id sit nisi laboris est elit mollit deserunt ex labore labore sunt. Anim laboris aliquip dolor sit et in.')
+                TypeWriterText(
+                  text: Text('maunel es un crack'), 
+                  duration: Duration(seconds: 9)
+                )
               ],
             );
           })
--- lib/widgets/widgets.dart
+++ lib/widgets/widgets.dart
@@ -2,6 +2,7 @@
 export 'package:personal_web/widgets/custom_sliverappbar.dart';
 export 'package:personal_web/widgets/home_scroll_view.dart';
 export 'package:personal_web/widgets/geometric_background.dart';
+export 'package:personal_web/widgets/typewritertext.dart';
 
 
 
