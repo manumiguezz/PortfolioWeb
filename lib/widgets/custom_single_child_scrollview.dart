69de2cee73204b876a3ba50542a61c7c01ff9c51
Manuel Miguez Lauría <manuelmiguezlauria@gmail.com>
2023-07-21 15:41:35 -0300

    single child scroll view separated

---
+++ lib/widgets/custom_single_child_scrollview.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class CustomSingleChildScrollView extends StatelessWidget {
+  const CustomSingleChildScrollView({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              
+              Text('Noteshop App',
+                style: TextStyle(fontFamily: 'madetommybold', fontSize: 50),
+                textAlign: TextAlign.start,
+                ),
+              
+              Text('Noteshop is a Dart/Flutter app designed to enhance your shopping experience. Integrated with the Teslo Shop backend, this app leverages Riverpod, Go Router, and CRUD REST API endpoints to provide a seamless shopping journey. With Noteshop, you can effortlessly browse and purchase products while enjoying the ability to add notes to your products.',
+                style: TextStyle(fontFamily: 'madetommy', fontSize: 50),
+                softWrap: true,
+                textAlign: TextAlign.justify,
+                ),
+
+            ],
+          )
+          
+        ],
+      ),
+    );
+  }
+}
