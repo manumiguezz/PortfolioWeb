c74b2f1a3539c11b6b33c000018ad8ec6a3015ec
Manuel Miguez Lauría <manuelmiguezlauria@gmail.com>
2023-07-21 16:43:05 -0300

    fonts changed

--- lib/widgets/custom_single_child_scrollview.dart
+++ lib/widgets/custom_single_child_scrollview.dart
@@ -18,20 +18,23 @@ class CustomSingleChildScrollView extends StatelessWidget {
             children: [
               
               const Expanded(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    
-                    Text('Noteshop App',
-                      style: TextStyle(fontFamily: 'madetommybold', fontSize: 50),
-                      textAlign: TextAlign.start,
-                      ),
-              
-                    Text('Noteshop is a Dart/Flutter app designed to enhance your shopping experience. Integrated with the Teslo Shop backend, this app leverages Riverpod, Go Router, and CRUD REST API endpoints to provide a seamless shopping journey. With Noteshop, you can effortlessly browse and purchase products while enjoying the ability to add notes to your products.',
-                      style: TextStyle(fontFamily: 'madetommy', fontSize: 15),
-                      textAlign: TextAlign.start,
-                      ),
-                  ],
+                child: Padding(
+                  padding: EdgeInsets.only(right: 1000, left: 120),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      
+                      Text('Noteshop App',
+                        style: TextStyle(fontFamily: 'madetommybold', fontSize: 50),
+                        textAlign: TextAlign.start,
+                        ),
+                              
+                      Text('Noteshop is a Dart/Flutter app designed to enhance your shopping experience. Integrated with the Teslo Shop backend, this app leverages Riverpod, Go Router, and CRUD REST API endpoints to provide a seamless shopping journey. With Noteshop, you can effortlessly browse and purchase products while enjoying the ability to add notes to your products.',
+                        style: TextStyle(fontFamily: 'madetommy', fontSize: 15),
+                        textAlign: TextAlign.justify,
+                        ),
+                    ],
+                  ),
                 ),
               ),
 
