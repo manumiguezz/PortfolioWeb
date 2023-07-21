c1b91729436c0c00b752b3ba1ae3714d7353e0d0
Manuel Miguez Lauría <manuelmiguezlauria@gmail.com>
2023-07-21 16:43:21 -0300

    google fonts added, sized changed

--- lib/widgets/custom_single_child_scrollview.dart
+++ lib/widgets/custom_single_child_scrollview.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
 
 class CustomSingleChildScrollView extends StatelessWidget {
   const CustomSingleChildScrollView({
@@ -17,30 +19,63 @@ class CustomSingleChildScrollView extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.start,
             children: [
               
-              const Expanded(
+              Expanded(
                 child: Padding(
-                  padding: EdgeInsets.only(right: 1000, left: 120),
+                  padding: const EdgeInsets.only(right: 100, left: 290),
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      
-                      Text('Noteshop App',
-                        style: TextStyle(fontFamily: 'madetommybold', fontSize: 50),
+
+                      Text('IOS & ANDROID',
+                        style: GoogleFonts.oswald(
+                          fontWeight: FontWeight.w600,
+                          height: 1.3,
+                          fontSize: 20, 
+                          color: Colors.white),
                         textAlign: TextAlign.start,
                         ),
+                      
+                      Text('NOTESHOP APP',
+                        style: GoogleFonts.oswald(
+                          fontWeight: FontWeight.w600,
+                          height: 1.3,
+                          fontSize: 50, 
+                          color: Colors.white),
+                        textAlign: TextAlign.start,
+                        ),
+
+                      const SizedBox(height: 8),
                               
-                      Text('Noteshop is a Dart/Flutter app designed to enhance your shopping experience. Integrated with the Teslo Shop backend, this app leverages Riverpod, Go Router, and CRUD REST API endpoints to provide a seamless shopping journey. With Noteshop, you can effortlessly browse and purchase products while enjoying the ability to add notes to your products.',
-                        style: TextStyle(fontFamily: 'madetommy', fontSize: 15),
+                      Text('Noteshop is a Dart & Flutter app designed to enhance your shopping experience. Integrated with the Teslo Shop backend, this app leverages Riverpod, Go Router, and CRUD REST API endpoints to provide a seamless shopping journey. With Noteshop, you can effortlessly browse and purchase products while enjoying the ability to add notes to your products.',
+                        style: GoogleFonts.inter(
+                          fontWeight: FontWeight.w300,
+                          height: 1.3,
+                          fontSize: 17, 
+                          color: Colors.white),
                         textAlign: TextAlign.justify,
                         ),
+
+                      const SizedBox(height: 25),
+
+                      ElevatedButton(
+                        onPressed: () {}, 
+                        child: const Text('EXPLORE MORE', style: TextStyle(fontWeight: FontWeight.bold),),
+                        style: ButtonStyle(
+                          fixedSize: MaterialStatePropertyAll(Size(180, 50))
+                        ),
+                      )
+
+                      
                     ],
                   ),
                 ),
               ),
 
-              Container(
-                color: Colors.blue,
-                height: 300,
+              Padding(
+                padding: const EdgeInsets.only(right: 290),
+                child: Placeholder(
+                  color: Colors.red,
+                ),
               )
             ],
           )
