60a3bad2a1b2b60bd1c58b59714a7082c3337143
Manuel Miguez Lauría <manuelmiguezlauria@gmail.com>
2023-07-25 14:23:21 -0300

    bug fixed

--- lib/widgets/listview.dart
+++ lib/widgets/listview.dart
@@ -1,18 +1,25 @@
 import 'package:flutter/material.dart';
-import 'package:personal_web/widgets/widgets.dart';
+import 'package:personal_web/widgets/parallax.dart';
 
 class PageListView extends StatelessWidget {
   const PageListView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: [
-        Parallax(),
+    return CustomScrollView(
+      slivers: [
+        
+        const SliverToBoxAdapter(
+          child: Parallax(),
+        ),
 
-        Container(
-          color: Colors.black,
-          height: 400,
+        SliverList(
+          delegate: SliverChildListDelegate([
+            Container(
+              height: 9000,
+              color: Colors.black,
+            )
+          ])
         )
       ],
     );
