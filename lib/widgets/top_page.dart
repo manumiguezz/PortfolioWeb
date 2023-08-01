6be499b4ca53f03f4cf2236e711f4d779dec9a12
Manuel Miguez Lauría <manuelmiguezlauria@gmail.com>
2023-08-01 20:17:55 -0300

    welcome presentation changed to presentations and changes on libraries

--- lib/widgets/top_page.dart
+++ lib/widgets/top_page.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'package:personal_web/widgets/animated_text.dart';
 import 'package:personal_web/widgets/custom_particle_background.dart';
-import 'package:personal_web/widgets/url_launcher.dart';
 import 'package:personal_web/widgets/widgets.dart';
 
 
@@ -18,85 +15,7 @@ class TopPage extends StatelessWidget {
 
         const SideVignette(),
 
-        Padding(
-          padding: const EdgeInsets.only(top: 200, left: 50),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-        
-              Row(
-                children: [
-                  Text(
-                    "Hi, I'm ", 
-                    style: GoogleFonts.poppins(
-                      fontWeight: FontWeight.w300,
-                      fontSize: 80,
-                      color: Colors.white
-                    ),
-                  ),
-
-                  Text(
-                    "Manuel Miguez,", 
-                    style: GoogleFonts.poppins(
-                      fontWeight: FontWeight.w700,
-                      fontSize: 80,
-                      color: Colors.white
-                    ),
-                  ),
-                ],
-              ),
-              
-              Row(
-                children: [
-                  Text(
-                    'a', 
-                    style: GoogleFonts.poppins(
-                      fontWeight: FontWeight.w300,
-                      fontSize: 80,
-                      color: Colors.white
-                    ),
-                  ),  
-                  const AnimatedText(speed: 70000,),
-                ],
-              ),
-
-              const SizedBox(height: 30,),
-
-              ElevatedButton(
-                onPressed: () => launchUrl('https://www.linkedin.com/in/manuelmiguezlauria/'),
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.transparent,
-                  side: const BorderSide(color: Colors.white, width: 3),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(2.0),
-                  ),
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 15),
-                  child: Row(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      Text(
-                        'CONTACT ME',
-                        style: GoogleFonts.poppins(
-                            fontWeight: FontWeight.w400,
-                            fontSize: 30,
-                            color: Colors.white,
-                          )
-                      ),
-
-                      const SizedBox(width: 40,),
-
-                      const Icon(Icons.keyboard_arrow_down_outlined, size: 40,)
-                    ],
-                  ),
-                ),
-              ),      
-            ],
-        
-          ),
-        ),
+        const Presentation(),
 
       ],
     );
--- lib/widgets/widgets.dart
+++ lib/widgets/widgets.dart
@@ -1,6 +1,8 @@
 
+export 'package:personal_web/widgets/presentation.dart';
 export 'package:personal_web/utilities/particle/flutter_particle_background.dart';
 export 'package:personal_web/widgets/side_vignette.dart';
 export 'package:personal_web/widgets/project_item.dart';
 export 'package:personal_web/widgets/url_launcher.dart';
+
 
