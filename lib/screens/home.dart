4bc5a991c7c1610594d01b70194eabae089e5ad5
Manuel Miguez Lauría <manuelmiguezlauria@gmail.com>
2023-08-02 10:42:35 -0300

    web scroll changes made

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -27,72 +27,76 @@ class _HomeScreenState extends State<HomeScreen> {
     return Scaffold(
       backgroundColor: Colors.black,
 
-      body: WebSmoothScroll(
-        controller: scrollController, 
-        child: SingleChildScrollView(
-          physics: const NeverScrollableScrollPhysics(),
+      body: Scrollbar(
+        child: WebSmoothScroll(
+          animationDuration: 500,
+          scrollOffset: 10,
           controller: scrollController, 
-          child: Column(
-            children: [
-
-              Stack(
-                children: [
-                
-                  const CustomParticleBackground(),
-
-                  const CustomParticleBackground(
-                    backgroundColor: Colors.transparent,
-                    blur: true,
-                    blurIntensity: 7,
-                    particleColor: Colors.white10,
-                    biggestSize: 1,
-                    numberOfParticles: 40,
-                    highestSpeed: 2,
-                    allFilled: true,
-                    smallestSize: 70,
-                  ),
-
-                  const SideVignette(),
-
-                  const Presentation(),
-
-                  const CustomParticleBackground(
-                    backgroundColor: Colors.transparent,
-                    blur: true,
-                    blurIntensity: 0,
-                    particleColor: Color.fromARGB(26, 42, 42, 42),
-                    biggestSize: 1,
-                    numberOfParticles: 10,
-                    highestSpeed: 2,
-                    allFilled: true,
-                    smallestSize: 70,
-                  ),
-
-                  const CustomParticleBackground(
-                    backgroundColor: Colors.transparent,
-                    blur: true,
-                    blurIntensity: 0,
-                    particleColor: Color.fromARGB(96, 112, 112, 112),
-                    biggestSize: 1,
-                    numberOfParticles: 14,
-                    highestSpeed: 0.7,
-                    allFilled: true,
-                    smallestSize: 30,
-                  ),
-
-                  Padding(
-                    padding: const EdgeInsets.only(top: 500),
-                    child: Stack(
-                      children: [
-                        Image.asset('assets/images/white_stroke_two.png')
-                      ],
+          child: SingleChildScrollView(
+            physics: const NeverScrollableScrollPhysics(),
+            controller: scrollController, 
+            child: Column(
+              children: [
+      
+                Stack(
+                  children: [
+                  
+                    const CustomParticleBackground(),
+      
+                    const CustomParticleBackground(
+                      backgroundColor: Colors.transparent,
+                      blur: true,
+                      blurIntensity: 7,
+                      particleColor: Colors.white10,
+                      biggestSize: 1,
+                      numberOfParticles: 40,
+                      highestSpeed: 2,
+                      allFilled: true,
+                      smallestSize: 70,
                     ),
-                  )
-
-                ],
-              ),
-
-            ],
+      
+                    const SideVignette(),
+      
+                    const Presentation(),
+      
+                    const CustomParticleBackground(
+                      backgroundColor: Colors.transparent,
+                      blur: true,
+                      blurIntensity: 0,
+                      particleColor: Color.fromARGB(26, 42, 42, 42),
+                      biggestSize: 1,
+                      numberOfParticles: 10,
+                      highestSpeed: 2,
+                      allFilled: true,
+                      smallestSize: 70,
+                    ),
+      
+                    const CustomParticleBackground(
+                      backgroundColor: Colors.transparent,
+                      blur: true,
+                      blurIntensity: 0,
+                      particleColor: Color.fromARGB(96, 112, 112, 112),
+                      biggestSize: 1,
+                      numberOfParticles: 14,
+                      highestSpeed: 0.7,
+                      allFilled: true,
+                      smallestSize: 30,
+                    ),
+      
+                    Padding(
+                      padding: const EdgeInsets.only(top: 500),
+                      child: Stack(
+                        children: [
+                          Image.asset('assets/images/white_stroke_two.png')
+                        ],
+                      ),
+                    )
+      
+                  ],
+                ),
+      
+              ],
+            ),
           ),
         ),
       ),
