ae0fdac8b7b0b0433f1255f8329f69b6b9fac81e
Manuel Miguez Lauría <manuelmiguezlauria@gmail.com>
2023-08-06 21:13:07 -0300

    particle group organized

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -81,20 +81,7 @@ class _HomeScreenState extends State<HomeScreen> {
                       smallestSize: 30,
                     ),
       
-                    Padding(
-                      padding: const EdgeInsets.only(top: 450,),
-                      child: Stack(
-                        children: [
-                          SizedBox(
-                            height: 1300,
-                            width: pageWidth,
-                            child: CustomPaint(
-                              painter: WhiterStrokePainter(),
-                            ),
-                          ),
-                        ],
-                      )
-                      ),
+                    AboutMe(pageWidth: pageWidth),
 
                     const Presentation(),
       
@@ -107,6 +94,65 @@ class _HomeScreenState extends State<HomeScreen> {
         ),
       ),
     );
+  }
+}
+
+class AboutMe extends StatelessWidget {
+  const AboutMe({
+    super.key,
+    required this.pageWidth,
+  });
+
+  final double pageWidth;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 450,),
+      child: Stack(
+        children: [
+
+          const Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Row(
+                children: [
+                  Text(
+                    'About me',
+                    style: TextStyle(
+                      fontFamily: 'poppinsbold',
+                      fontSize: 50,
+                      color: Colors.black
+                    ),
+                  ),
+
+                  Text(
+                    softWrap: true,
+                    overflow: TextOverflow.clip,
+                    "As an aspiring backend software developer, I have a strong foundation in Java development, with expertise in Java 11 and 17. I'm proficient in object-oriented programming and can work effectively with other languages like Dart, C, C++, as well as web technologies such as HTML and CSS. I also have extensive knowledge of frameworks like Spring Boot, Flutter, Hibernate, and JavaFX, enabling me to work on diverse projects. I've developed RESTful APIs with CRUD operations and established database connections. Additionally, I'm skilled in using dependency managers like Maven and Gradle for efficient project management and organization.",
+                    style: TextStyle(
+                      fontFamily: 'poppinsregular',
+                      fontSize: 50,
+                      color: Colors.black
+                    ),
+                  ),
+
+                ],
+              )
+            ],
+          ),
+          
+          SizedBox(
+            height: 1300,
+            width: pageWidth,
+            child: CustomPaint(
+              painter: WhiterStrokePainter(),
+            ),
+          ),
+        ],
+      )
+      );
   }
 }
 
