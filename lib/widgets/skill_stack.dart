082b0fa0d1bebfdfc81d7bb5f68fb4b2129909df
Manuel Miguez Lauría <manuelmiguezlauria@gmail.com>
2023-08-19 00:01:36 -0300

    width deleted, replaced with padding

--- lib/widgets/skill_stack.dart
+++ lib/widgets/skill_stack.dart
@@ -26,31 +26,37 @@ class MyStack extends StatelessWidget {
               alignment: Alignment.center,
               children: [
                 Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(17)
+                  ),
                   child: Padding(
-                    padding: const EdgeInsets.all(8.0),
+                    padding: const EdgeInsets.all(16),
                     child: Image.asset(
                       'assets/icons/techstack/json.png',
                       scale: 6,
                     ),
                   ),
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(17)
-                  ),
                 ),
                 Padding(
                   padding: const EdgeInsets.only(top: 115),
                   child: Container(
-                    width: 75,
                     decoration: BoxDecoration(
                       color: Colors.white,
                       borderRadius: BorderRadius.circular(13),
                       border: Border.all(color: Colors.black, width: 4)
                     ),
-                    child: const Text(
-                      'data', 
-                      textAlign: TextAlign.center,
-                      style: TextStyle(fontSize: 20, color: Colors.black)),
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 3),
+                      child: const Text(
+                        'JSON', 
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 20, 
+                          color: Colors.black,
+                          fontFamily: 'poppinsbold',                        
+                        )),
+                    ),
                   ),
                 )
               ],
