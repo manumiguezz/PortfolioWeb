7441a729a556babc773687955541cb3d0e9f91d8
Manuel Miguez Lauría <manuelmiguezlauria@gmail.com>
2023-08-19 20:18:36 -0300

    my stack paddings and sized box added

--- lib/widgets/skill_stack.dart
+++ lib/widgets/skill_stack.dart
@@ -10,30 +10,77 @@ class MyStack extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Column(
       children: [
+
         Text(
           'My Stack',
           style: TextStyle(
             color: Colors.white,
             fontFamily: 'poppinsbold',
-            fontSize: 70
+            fontSize: 60
           ),
         ),
 
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
+        SizedBox(height: 20,),
 
-            TechContainer(asset: 'json.png', techName: 'JSON'),
-            TechContainer(asset: 'json.png', techName: 'JSON'),
-            TechContainer(asset: 'json.png', techName: 'JSON'),
-            TechContainer(asset: 'json.png', techName: 'JSON'),
-            TechContainer(asset: 'json.png', techName: 'JSON'),
-            TechContainer(asset: 'json.png', techName: 'JSON'),
+        SizedBox(
+          height: 500,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 200),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+              
+                    TechContainer(asset: 'java.png', techName: 'JAVA'),
+                    TechContainer(asset: 'c.png', techName: 'C'),
+                    TechContainer(asset: 'dart.png', techName: 'DART'),
+                    TechContainer(asset: 'html.png', techName: 'HTML'),
+                    TechContainer(asset: 'css.png', techName: 'CSS'),
+                    TechContainer(asset: 'mysql.png', techName: 'MYSQL'),
+              
+                  ],
+                ),
+              ),
 
-          ],
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 200),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                  
+                    TechContainer(asset: 'spring.png', techName: 'SPRING'),
+                    TechContainer(asset: 'javafx.png', techName: 'JAVAFX'),
+                    TechContainer(asset: 'hibernate.png', techName: 'HIBERNATE'),
+                    TechContainer(asset: 'springboot.png', techName: 'SPRING BOOT'),
+                    TechContainer(asset: 'flutter.png', techName: 'FLUTTER'),
+                    TechContainer(asset: 'git.png', techName: 'GIT'),
+
+                  ],
+                ),
+              ),
+
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 200),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                  
+                    TechContainer(asset: 'postman.png', techName: 'POSTMAN'),
+                    TechContainer(asset: 'maven.png', techName: 'MAVEN'),
+                    TechContainer(asset: 'thymeleaf.png', techName: 'THYMELEAF'),
+                    TechContainer(asset: 'docker.png', techName: 'DOCKER'),
+                    TechContainer(asset: 'firebase.png', techName: 'FIREBASE'),
+                    TechContainer(asset: 'json.png', techName: 'JSON'),
+
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
 
- 
       ],
     );
   }
