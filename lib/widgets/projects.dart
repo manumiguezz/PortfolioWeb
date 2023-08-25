ad780ba21c74e23d82103eb26270d1b0c03952f7
Manuel Miguez Lauría <manuelmiguezlauria@gmail.com>
2023-08-24 21:54:01 -0300

    noteshop app added

--- lib/widgets/projects.dart
+++ lib/widgets/projects.dart
@@ -9,6 +9,7 @@ class ProjectSection extends StatelessWidget {
       children: [
 
         Text(
+          textAlign: TextAlign.center,
           'My Stack',
           style: TextStyle(
             color: Colors.white,
@@ -17,7 +18,46 @@ class ProjectSection extends StatelessWidget {
           ),
         ),
 
+        SizedBox(height: 50),
+
+        Padding(
+          padding: EdgeInsets.only(left: 70 ),
+          child: Row(
+            children: [
+              Column(
+                children: [
+                  
+                  Text(
+                    'Noteshop App',
+                    style: TextStyle(
+                      fontFamily: 'poppinsbold',
+                      fontSize: 70,
+                      color: Colors.white
+                    ),
+                  ),
         
+                  SizedBox(height: 20),
+        
+                  SizedBox(
+                    width: 350,
+                    child: Text(
+                      'Noteshop is a Dart/Flutter app designed to enhance your shopping experience. Integrated with the Teslo Shop backend, this app leverages Riverpod, Go Router, and CRUD REST API endpoints to provide a seamless shopping journey. With Noteshop, you can effortlessly browse and purchase products while enjoying the ability to add notes to your products.',
+                      softWrap: true,
+                      textAlign: TextAlign.justify,
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontFamily: 'poppinslight',
+                          fontSize: 20,
+                      )
+                    ),
+                  )
+                ],
+              )
+            ],
+          ),
+        )
+
+
       ],
       
     );
