99d3ee052904a17d3cbc0d32795826b01629a5e5
Manuel Miguez Lauría <manuelmiguezlauria@gmail.com>
2023-08-25 15:06:37 -0300

    building noteshop, assets to be changed

--- lib/widgets/projects.dart
+++ lib/widgets/projects.dart
@@ -117,65 +117,72 @@ class NoteshopApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.only(left: 70 ),
-      child: Row(
+      padding: const EdgeInsets.only(left: 70,),
+      child: Stack(
         children: [
-          
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              
-              const Text(
-                'Noteshop',
-                style: TextStyle(
-                  fontFamily: 'poppinsbold',
-                  fontSize: 70,
-                  color: Colors.white
-                ),
-              ),
-
-              SizedBox(
-                width: 250,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Image.asset('assets/icons/techstackcolor/flutter.png', scale: 20,),
-                    Image.asset('assets/icons/techstackcolor/dart.png', scale: 20,),
-                    Image.asset('assets/icons/techstackcolor/jwt.png', scale: 20,),
-                    Image.asset('assets/icons/techstackcolor/http.png', scale: 20,),
-                    Image.asset('assets/icons/techstackcolor/restapi.png', scale: 20,),
-                  ],
-                ),
-              ),
-    
-              const SizedBox(height: 20),
-    
-              const SizedBox(
-                width: 510,
-                child: Text(
-                  'Noteshop is a Dart/Flutter app designed to enhance your shopping experience. Integrated with the Teslo Shop backend, this app leverages Riverpod, Go Router, and CRUD REST API endpoints to provide a seamless shopping journey. With Noteshop, you can effortlessly browse and purchase products while enjoying the ability to add notes to your products.',
-                  softWrap: true,
-                  textAlign: TextAlign.justify,
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontFamily: 'poppinslight',
-                      fontSize: 20,
-                  )
-                ),
-              )
-
-            ],
-          ),
-
-          const SizedBox(width: 300,),
 
           Padding(
-            padding: const EdgeInsets.only(top: 60),
-            child: Image.asset(
-              'assets/images/project/noteshop_white.png',
-              scale: 7,
+            padding: const EdgeInsets.only(top: 0, ),
+            child: Transform(
+              transform: Matrix4.translationValues(0, 0, 0),
+              child: Image.asset('assets/images/project/noteshop_white.png', scale: 4.6,), 
             ),
-          )
+          ),
+
+          const Row(
+            children: [
+            
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+
+                  SizedBox(height: 250,),
+            
+                  Text(
+                    'Noteshop',
+                    style: TextStyle(
+                      fontFamily: 'poppinsbold',
+                      fontSize: 70,
+                      color: Colors.white
+                    ),
+                  ),
+            
+                  SizedBox(
+                    width: 330,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text('FLUTTER', style: TextStyle(fontFamily: 'poppinsregular', fontSize: 15, color: Colors.white)),
+                        Text('DART', style: TextStyle(fontFamily: 'poppinsregular', fontSize: 15, color: Colors.white)),
+                        Text('JWT', style: TextStyle(fontFamily: 'poppinsregular', fontSize: 15, color: Colors.white)),
+                        Text('HTTP', style: TextStyle(fontFamily: 'poppinsregular', fontSize: 15, color: Colors.white)),
+                        Text('RESTAPI', style: TextStyle(fontFamily: 'poppinsregular', fontSize: 15, color: Colors.white)),
+                      ],
+                    ),
+                  ),
+            
+                  SizedBox(height: 20),
+            
+                  SizedBox(
+                    width: 420,
+                    child: Text(
+                      'Noteshop is a Dart/Flutter app designed to enhance your shopping experience. Integrated with the Teslo Shop backend, this app leverages Riverpod, Go Router, and CRUD REST API endpoints to provide a seamless shopping journey. With Noteshop, you can effortlessly browse and purchase products while enjoying the ability to add notes to your products.',
+                      softWrap: true,
+                      textAlign: TextAlign.justify,
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontFamily: 'poppinslight',
+                          fontSize: 20,
+                      )
+                    ),
+                  )
+            
+                ],
+              ),
+            
+            ]
+          ),
+          
         ],
       ),
     );
