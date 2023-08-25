13033ca3abe0d382f29c69ce153e21c33c39c7a2
Manuel Miguez Lauría <manuelmiguezlauria@gmail.com>
2023-08-25 01:02:20 -0300

    flickframes finished, buttons to be added

--- lib/widgets/projects.dart
+++ lib/widgets/projects.dart
@@ -39,66 +39,71 @@ class Flickframes extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.only(left: 70),
-      child: SizedBox(
-        width: MediaQuery.of(context).size.width,
-        child: Row(
-          children: [
-          
-            const Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-          
-                Text(
-                  'Flickframes',
-                  style: TextStyle(
-                    fontFamily: 'poppinsbold',
-                    fontSize: 70,
-                    color: Colors.white
-                  ),
-                ),
-          
-                SizedBox(
-                  width: 330,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text('FLUTTER', style: TextStyle(fontFamily: 'poppinsregular', fontSize: 15, color: Colors.white)),
-                      Text('DART', style: TextStyle(fontFamily: 'poppinsregular', fontSize: 15, color: Colors.white)),
-                      Text('ISARDB', style: TextStyle(fontFamily: 'poppinsregular', fontSize: 15, color: Colors.white)),
-                      Text('THEMOVIEDB', style: TextStyle(fontFamily: 'poppinsregular', fontSize: 15, color: Colors.white)),
-                    ],
-                  ),
-                ),
-          
-                SizedBox(height: 20),
-          
-                SizedBox(
-                  width: 510,
-                  child: Text(
-                    'FlickerFrames is an entertainment app designed for movie enthusiasts, providing a comprehensive collection of movies, including information about vote averages, descriptions, and cast details. With FlickerFrames, you can explore a wide range of movies and discover similar titles that match your interests.',
-                    softWrap: true,
-                    textAlign: TextAlign.justify,
+      padding: const EdgeInsets.only(left: 70,),
+      child: Stack(
+        children: [
+          const Row(
+            children: [
+            
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+
+                  SizedBox(height: 250,),
+            
+                  Text(
+                    'Flickframes',
                     style: TextStyle(
-                        color: Colors.white,
-                        fontFamily: 'poppinslight',
-                        fontSize: 20,
-                    )
+                      fontFamily: 'poppinsbold',
+                      fontSize: 70,
+                      color: Colors.white
+                    ),
                   ),
-                )
-          
-              ],
-            ),
-          
-            Padding(
-              padding: const EdgeInsets.only(top: 60, right: 5),
-              child: Transform(
-                transform: Matrix4.translationValues(-50, 0, 0), // Translate image by -50 pixels on X-axis
-                child: Image.asset('assets/images/project/flickframes_white.png', scale: 4.8,), 
+            
+                  SizedBox(
+                    width: 330,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text('FLUTTER', style: TextStyle(fontFamily: 'poppinsregular', fontSize: 15, color: Colors.white)),
+                        Text('DART', style: TextStyle(fontFamily: 'poppinsregular', fontSize: 15, color: Colors.white)),
+                        Text('ISARDB', style: TextStyle(fontFamily: 'poppinsregular', fontSize: 15, color: Colors.white)),
+                        Text('THEMOVIEDB', style: TextStyle(fontFamily: 'poppinsregular', fontSize: 15, color: Colors.white)),
+                      ],
+                    ),
+                  ),
+            
+                  SizedBox(height: 20),
+            
+                  SizedBox(
+                    width: 420,
+                    child: Text(
+                      'FlickerFrames is an entertainment app designed for movie enthusiasts, providing a comprehensive collection of movies, including information about vote averages, descriptions, and cast details. With FlickerFrames, you can explore a wide range of movies and discover similar titles that match your interests.',
+                      softWrap: true,
+                      textAlign: TextAlign.justify,
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontFamily: 'poppinslight',
+                          fontSize: 20,
+                      )
+                    ),
+                  )
+            
+                ],
               ),
-            )
-          ]
-        ),
+            
+            ]
+          ),
+          
+          Padding(
+            padding: const EdgeInsets.only(top: 0, ),
+            child: Transform(
+              transform: Matrix4.translationValues(410, 0, 0),
+              child: Image.asset('assets/images/project/flickframes_white.png', scale: 4.6,), 
+            ),
+          )
+          
+        ],
       ),
     );
   }
