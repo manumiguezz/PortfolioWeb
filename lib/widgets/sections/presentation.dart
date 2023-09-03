7d4be79e8beed35d370ffbdb992a0357b6a2e1f9
Manuel Miguez Lauría <manuelmiguezlauria@gmail.com>
2023-09-03 16:41:27 -0300

    email recovered

--- lib/widgets/sections/presentation.dart
+++ lib/widgets/sections/presentation.dart
@@ -9,8 +9,6 @@ class Presentation extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    bool isHovered = false;
-
     return Padding(
       padding: const EdgeInsets.only(top: 200, left: 50),
       child: Column(
@@ -80,26 +78,11 @@ class Presentation extends StatelessWidget {
 
 
               
-              MouseRegion(
-                cursor: SystemMouseCursors.click,
-                onEnter: (_) {},
-                onExit: (_) {},
-                child: AnimatedSize(
-                  duration: const Duration(milliseconds: 300),
-                  curve: Curves.easeInOut,
-                  child: Container(
-                    
-                    width: isHovered ? 100.0 : 80.0,
-                    height: isHovered ? 100.0 : 80.0,
-                    child: GestureDetector(
-                      onTap: () => launchUrl('mailto:manuelmiguezlauria@gmail.com'),
-                      child: Image.asset(
-                        'assets/icons/socialmedia/email_black.png',
-                        color: Colors.white,
-                        scale: 11,
-                      ),
-                    ),
-                  ),
+              GestureDetector(
+                onTap: () => launchUrl('mailto:manuelmiguezlauria@gmail.com'),
+                child: MouseRegion(
+                  cursor: MaterialStateMouseCursor.clickable,
+                  child: Image.asset('assets/icons/socialmedia/email_black.png', color: Colors.white, scale: 11,)
                 ),
               ),
 
