1469467f84827ecb20123abd70dc5249546d2bc6
Manuel Miguez <manuelmiguezlauria@gmail.com>
2023-10-15 18:01:55 -0300

    exapmle copied

--- lib/screens/pdf_viewer.dart
+++ lib/screens/pdf_viewer.dart
@@ -1,13 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_pdfviewer/pdfviewer.dart';
 
-class PdfViewer extends StatelessWidget {
-  const PdfViewer({super.key});
+class PdfViewer extends StatefulWidget {
+  @override
+  _PdfViewer createState() => _PdfViewer();
+}
+
+class _PdfViewer extends State<PdfViewer> {
+  final GlobalKey<SfPdfViewerState> _pdfViewerKey = GlobalKey();
+
+  @override
+  void initState() {
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        child: PdfViewer(),
+      appBar: AppBar(
+        title: const Text('Syncfusion Flutter PDF Viewer'),
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(
+              Icons.bookmark,
+              color: Colors.white,
+              semanticLabel: 'Bookmark',
+            ),
+            onPressed: () {
+              _pdfViewerKey.currentState?.openBookmarkView();
+            },
+          ),
+        ],
+      ),
+      body: SfPdfViewer.network(
+        'https://cdn.syncfusion.com/content/PDFViewer/flutter-succinctly.pdf',
+        key: _pdfViewerKey,
       ),
     );
   }
