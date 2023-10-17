fbd87cdeeda3bbfb18a9f3174a1a57239ad08ff3
Manuel Miguez <manuelmiguezlauria@gmail.com>
2023-10-16 23:50:28 -0300

    initial zoom level set, padding added

--- lib/screens/pdf_viewer.dart
+++ lib/screens/pdf_viewer.dart
@@ -9,21 +9,43 @@ class PdfViewer extends StatefulWidget {
 class _PdfViewer extends State<PdfViewer> {
   final GlobalKey<SfPdfViewerState> _pdfViewerKey = GlobalKey();
 
+  late PdfViewerController pdfViewerController;
+
   @override
   void initState() {
+    pdfViewerController = PdfViewerController();
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.black,
-        title: const Text('Manuel Miguez Lauría Resume'),
-        actions: const <Widget>[
-        ],
+
+    pdfViewerController.zoomLevel = 2;
+
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 170),
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.black,
+          title: const Text('Manuel Miguez Lauría Resume'),
+          actions: <Widget>[
+            IconButton(
+            icon: Icon(
+              Icons.zoom_in,
+              color: Colors.white,
+            ),
+            onPressed: () {
+              pdfViewerController.zoomLevel = 2;
+            },
+          ),
+          ],
+        ),
+        body: SfPdfViewer.asset(
+          'assets/pdf/resume.pdf',
+          controller: pdfViewerController,
+          initialZoomLevel: 0.4,
+        )
       ),
-      body: SfPdfViewer.asset('assets/pdf/resume.pdf')
     );
   }
 }
