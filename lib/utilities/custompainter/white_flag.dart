7fde5bfbc92557ddf967d72d81697a7e02919d82
Manuel Miguez <manuelmiguezlauria@gmail.com>
2023-10-27 18:09:48 -0300

    white stroke adapted advanced

--- lib/utilities/custompainter/white_flag.dart
+++ lib/utilities/custompainter/white_flag.dart
@@ -18,7 +18,8 @@ class WhiteFlagSection extends StatelessWidget {
     late bool mobileVersion = false;
 
     if (heightQuery < 100) {
-      
+      paddingTop = heightQuery * 0.54;
+      pageHeightStroke = heightQuery * 0.9;
     } else if (heightQuery < 200) {
 
     } else if (heightQuery < 300) {
@@ -59,7 +60,6 @@ class WhiteFlagSection extends StatelessWidget {
       
       mobileVersion = true;
     } else if (widthQuery < 400) {
-
       List<double> heightThresholds = [
         420, 450, 470, 500, 540, 580, 610, 650, 690, 740, 790, 850, 920, 980, 1050, 1100
       ];
@@ -110,40 +110,104 @@ class WhiteFlagSection extends StatelessWidget {
       mobileVersion = true;
       
     } else if (widthQuery < 600) {
-        if (heightQuery < 300) {
-          paddingTop = heightQuery * 0.49;
-          pageHeightStroke = heightQuery * 1.25;
-        } else if (heightQuery < 200) {
-          paddingTop = heightQuery * 0.49;
-          pageHeightStroke = heightQuery * 1.25;
-        } else if (heightQuery < 300) {
+      List<double> heightThresholds = [
+        310, 335, 360, 390, 420, 450, 490, 530, 580, 640, 700, 770, 830, 910, 980, 1050, 1120, 1220, 1340
+      ];
 
-        } else if (heightQuery < 400) {
+      List<double> paddingTopValues = [
+        1.15, 1.05, 1.0, 0.9, 0.85, 0.82, 0.80, 0.77, 0.73, 0.68, 0.62, 0.57, 0.54, 0.52, 0.5, 0.48, 0.46, 0.43, 0.40
+      ];
 
-        } else if (heightQuery < 500) {
+      List<double> pageHeightStrokeValues = [
+        3.1, 3.0, 2.8, 2.65, 2.45, 2.28, 2.05, 1.85, 1.68, 1.55, 1.38, 1.30, 1.20, 1.08, 1.0, 0.92, 0.86, 0.81, 0.75
+      ];
 
-        } else if (heightQuery < 600) {
+      int index = heightThresholds.indexWhere((threshold) => heightQuery < threshold);
 
-        } else if (heightQuery < 700) {
-
-        } else if (heightQuery < 800) {
-
-        } else if (heightQuery < 900) {
-
-        } else if (heightQuery < 1000) {
-
-        }
+      if (index != -1) {
+        paddingTop = heightQuery * paddingTopValues[index];
+        pageHeightStroke = heightQuery * pageHeightStrokeValues[index];
+      } else {
+        paddingTop = 0;
+        pageHeightStroke = 0;
+      }
       
       mobileVersion = true;
+
     } else if (widthQuery < 700) {
-      paddingTop = heightQuery * 0.54;
-      pageHeightStroke = heightQuery * 0.8;
+      List<double> heightThresholds = [
+        500, 550, 650, 760, 880, 1000, 1150, 1220, 9999
+      ];
+
+      List<double> paddingTopValues = [
+        1, 0.7, 0.65, 0.60, 0.55, 0.50, 0.42, 0.38, 0.30
+      ];
+
+      List<double> pageHeightStrokeValues = [
+        1.3, 1.8, 1.4, 1.2, 1, 0.8, 0.75, 0.60, 0.60
+      ];
+
+      int index = heightThresholds.indexWhere((threshold) => heightQuery < threshold);
+
+      if (index != -1) {
+        paddingTop = heightQuery * paddingTopValues[index];
+        pageHeightStroke = heightQuery * pageHeightStrokeValues[index];
+      } else {
+        paddingTop = 0;
+        pageHeightStroke = 0;
+      }
+      mobileVersion = false;
+
     } else if (widthQuery < 800) {
-      paddingTop = heightQuery * 0.54;
-      pageHeightStroke = heightQuery * 0.9;
+      List<double> heightThresholds = [
+      500, 550, 580, 630, 690, 740, 820, 890, 960, 1050, 1150, 1240, 1300
+    ];
+
+    List<double> paddingTopValues = [
+      0.54, 0.80, 0.75, 0.7, 0.65, 0.6, 0.55, 0.5, 0.45, 0.4, 0.35, 0.35, 0.32
+    ];
+
+    List<double> pageHeightStrokeValues = [
+      0.9, 1.4, 1.3, 1.2, 1.1, 1, 0.9, 0.85, 0.8, 0.75, 0.7, 0.62, 0.60
+    ];
+
+    int index = heightThresholds.indexWhere((threshold) => heightQuery < threshold);
+
+    if (index != -1) {
+      paddingTop = heightQuery * paddingTopValues[index];
+      pageHeightStroke = heightQuery * pageHeightStrokeValues[index];
+    } else {
+      paddingTop = 0;
+      pageHeightStroke = 0;
+    }
+      mobileVersion = false;
+
     } else if (widthQuery < 900) {
-      paddingTop = heightQuery * 0.58;
-      pageHeightStroke = heightQuery * 1.12;
+      if (heightQuery < 500) {
+        paddingTop = heightQuery * 1.15;
+        pageHeightStroke = heightQuery * 1.35;
+      } else if (heightQuery < 9999) {
+        paddingTop = heightQuery * 1.15;
+        pageHeightStroke = heightQuery * 1.35;
+      } else if (heightQuery < 9999) {
+
+      } else if (heightQuery < 9999) {
+
+      } else if (heightQuery < 9999) {
+
+      } else if (heightQuery < 9999) {
+
+      } else if (heightQuery < 9999) {
+
+      } else if (heightQuery < 9999) {
+
+      } else if (heightQuery < 9999) {
+
+      } else if (heightQuery < 9999) {
+
+      }
+      
+      mobileVersion = false;
     } else if (widthQuery < 1000) {
       paddingTop = heightQuery * 0.60;
       pageHeightStroke = heightQuery * 1.1;
