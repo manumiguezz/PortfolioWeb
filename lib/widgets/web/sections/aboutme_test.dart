f2329fd57a8ccd071b07001d69f7cff462b947bb
Manuel Miguez <manuelmiguezlauria@gmail.com>
2023-11-21 10:51:07 -0300

    about me group file created

--- lib/widgets/web/sections/aboutme_test.dart
+++ lib/widgets/web/sections/aboutme_test.dart
@@ -28,39 +28,9 @@ class AboutMeTest extends StatelessWidget {
 
         Container(
           color: Colors.white,
-          padding: EdgeInsets.symmetric(horizontal: widthQuery * 0.15),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.end,
             children: [
-
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: widthQuery * 0.15),
-                child: Row(
-                  children: [
-                    Text( 
-                      'About me',
-                      style: TextStyle(
-                        fontFamily: 'poppinsbold',
-                        fontSize: widthQuery * 0.07,
-                        color: Colors.black
-                      ),
-                    ),
-              
-                    Text(
-                      "As an aspiring backend software developer, I have a strong foundation in Java development, with expertise in Java 11 and 17. I'm proficient in object-oriented programming and can work effectively with other languages like Dart, C, as well as web technologies such as HTML and CSS.\n\n"
-                      "I also have extensive knowledge of frameworks like Spring Boot, Flutter, Hibernate, and JavaFX, enabling me to work on diverse projects. I've developed RESTful APIs with CRUD operations and established database connections. Additionally, I'm skilled in using dependency managers like Maven and Gradle for efficient project management and organization.\n\n"
-                      "Beyond my technical expertise, I possess effective and friendly communication skills, enabling seamless integration within teams. I easily adapt to new languages and tools, allowing me to contribute to a variety of projects. And employing critical thinking, I approach problem-solving methodically, while my confidence empowers me to confront challenges with determination.",
-                      textAlign: TextAlign.justify,
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontFamily: 'poppinslight',
-                        fontSize: fontSize
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-
               
 
               SizedBox(height: heightQuery * 0.04,),
