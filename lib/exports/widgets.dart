609ab6bbc2b197f0968e0df45eb8517358133a70
Manuel Miguez <manuelmiguezlauria@gmail.com>
2023-11-23 13:14:17 -0300

    exports widget file updated

--- lib/exports/widgets.dart
+++ lib/exports/widgets.dart
@@ -1,21 +1,16 @@
+
+export 'package:portfolio_web_version/widgets/hover_switch_mobile.dart';
+export 'package:portfolio_web_version/widgets/arrow_icon.dart';
+export 'package:portfolio_web_version/widgets/custom_animated_button.dart';
 export 'package:portfolio_web_version/widgets/social_button.dart';
 export 'package:portfolio_web_version/widgets/animated_text.dart';
-export 'package:portfolio_web_version/web/projects/companyrestapi.dart';
-export 'package:portfolio_web_version/web/projects/flickframes.dart';
-export 'package:portfolio_web_version/web/projects/noteshopapp.dart';
-export 'package:portfolio_web_version/web/sections/about_me.dart';
-export 'package:portfolio_web_version/web/sections/last_info.dart';
-export 'package:portfolio_web_version/web/sections/presentation.dart';
-export 'package:portfolio_web_version/web/sections/projects_section.dart';
-export 'package:portfolio_web_version/web/sections/skill_stack.dart';
 export 'package:portfolio_web_version/widgets/responsive_image.dart';
 export 'package:portfolio_web_version/widgets/tech_stack.dart';
 export 'package:portfolio_web_version/widgets/hover_switch.dart';
-export 'package:portfolio_web_version/utilities/custompainter/white_flag.dart';
 export 'package:portfolio_web_version/widgets/particle_group.dart';
 export 'package:portfolio_web_version/widgets/custom_particle_background.dart';
-export 'package:portfolio_web_version/utilities/particle/flutter_particle_background.dart';
 export 'package:portfolio_web_version/widgets/url_launcher.dart';
+
 
 
 
