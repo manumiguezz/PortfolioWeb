a26b6a0769b57455b3d54c58583cd315b3b1ffda
Manuel Miguez <manuelmiguezlauria@gmail.com>
2023-11-23 13:08:21 -0300

    exports folder created

---
+++ lib/exports/projects.dart
@@ -0,0 +1,7 @@
+import 'package:portfolio_web_version/mobile/projects/companyrestapi_mobile.dart';
+import 'package:portfolio_web_version/mobile/projects/flickframes_mobile.dart';
+import 'package:portfolio_web_version/mobile/projects/noteshopapp_mobile.dart';
+import 'package:portfolio_web_version/web/projects/companyrestapi.dart';
+import 'package:portfolio_web_version/web/projects/flickframes.dart';
+import 'package:portfolio_web_version/web/projects/noteshopapp.dart';
+
