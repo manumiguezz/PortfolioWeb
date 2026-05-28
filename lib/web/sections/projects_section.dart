2bd6a501969e871e643efa20cc53e0f0f901e82b
Manumiguezz <manuelmiguezlauria@gmail.com>
2026-05-27 23:45:58 -0300

    Increase project section breathing room

--- lib/web/sections/projects_section.dart
+++ lib/web/sections/projects_section.dart
@@ -10,9 +10,14 @@ class ProjectSection extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     double widthQuery = MediaQuery.of(context).size.width;
+    final isStacked = isNarrowDesktopWidth(widthQuery);
     final sectionTitleSize = clampSize(widthQuery * 0.07, 52, 96);
-    final introGap = clampSize(widthQuery * 0.075, 68, 128);
-    final projectGap = clampSize(widthQuery * 0.08, 64, 116);
+    final introGap = isStacked
+        ? clampSize(widthQuery * 0.17, 148, 196)
+        : clampSize(widthQuery * 0.075, 68, 128);
+    final projectGap = isStacked
+        ? clampSize(widthQuery * 0.18, 156, 208)
+        : clampSize(widthQuery * 0.08, 64, 116);
 
     return Column(
       children: [
