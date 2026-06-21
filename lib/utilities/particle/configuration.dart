fb7f26c991b26e114c76ca02eb97b2d51e7f8aeb
Manumiguezz <manuelmiguezlauria@gmail.com>
2026-06-21 01:09:57 -0300

    Stabilize particle configuration updates

--- lib/utilities/particle/configuration.dart
+++ lib/utilities/particle/configuration.dart
@@ -1,17 +1,17 @@
 import 'package:flutter/material.dart';
 
 class Configuration {
-  bool multiColor;
-  Color particleColor;
-  Color backgroundColor;
-  int numberOfParticles;
-  bool blur;
-  double highestSpeed;
-  double slowestSpeed;
-  int biggestSize;
-  int smallestSize;
-  bool allFilled;
-  int blurIntensity;
+  final bool multiColor;
+  final Color particleColor;
+  final Color backgroundColor;
+  final int numberOfParticles;
+  final bool blur;
+  final double highestSpeed;
+  final double slowestSpeed;
+  final int biggestSize;
+  final int smallestSize;
+  final bool allFilled;
+  final int blurIntensity;
 
   Configuration(
       {required this.multiColor,
@@ -25,4 +25,36 @@ class Configuration {
       required this.smallestSize,
       required this.blurIntensity,
       required this.allFilled});
+
+  @override
+  bool operator ==(Object other) {
+    return identical(this, other) ||
+        other is Configuration &&
+            other.multiColor == multiColor &&
+            other.particleColor == particleColor &&
+            other.backgroundColor == backgroundColor &&
+            other.numberOfParticles == numberOfParticles &&
+            other.blur == blur &&
+            other.highestSpeed == highestSpeed &&
+            other.slowestSpeed == slowestSpeed &&
+            other.biggestSize == biggestSize &&
+            other.smallestSize == smallestSize &&
+            other.allFilled == allFilled &&
+            other.blurIntensity == blurIntensity;
+  }
+
+  @override
+  int get hashCode => Object.hash(
+        multiColor,
+        particleColor,
+        backgroundColor,
+        numberOfParticles,
+        blur,
+        highestSpeed,
+        slowestSpeed,
+        biggestSize,
+        smallestSize,
+        allFilled,
+        blurIntensity,
+      );
 }
